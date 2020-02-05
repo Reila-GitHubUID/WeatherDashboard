31474d71a474c1a193de6cb9553e3d705ed19830
Ellin Tj <tjellin@gmail.com>
2020-02-05 14:01:52 -0800

    added the api url with an API key

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,6 @@
 let inputCity = $(".searchField");
 
-let url = "" + "";
+let url = "api.openweathermap.org/data/2.5/weather?q="+inputCity+"&APPID=4c5b7de512dad1fed533c8bdb4858956";
 
 $(document).ready(function () { 
     $.ajax({
