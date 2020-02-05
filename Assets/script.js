c38bd30ca23d2a08ab3585ccbcbfdccf388514b5
Ellin Tj <tjellin@gmail.com>
2020-02-05 13:54:55 -0800

    added ajax in the file

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,12 @@
+let inputCity = $(".searchField");
+
+let url = "" + "";
+
 $(document).ready(function () { 
-    
+    $.ajax({
+        url,
+        method: "GET"
+    }).then (function(response) {
+        console.log(response);
+    });
 });
