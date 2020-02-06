036bc69a0dae8e5d474122d7bcf5f2b71283d4dd
Ellin Tj <tjellin@gmail.com>
2020-02-05 19:02:31 -0800

    forgot to save my work from hours ago

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,6 @@
 let inputCity = $(".searchField");
 
-let url = "api.openweathermap.org/data/2.5/weather?q="+inputCity+"&APPID=4c5b7de512dad1fed533c8bdb4858956";
+let url = "api.openweathermap.org/data/2.5/weather?q="+"Castro Valley"+"&APPID=4c5b7de512dad1fed533c8bdb4858956";
 
 $(document).ready(function () { 
     $.ajax({
