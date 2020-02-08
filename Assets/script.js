b73503455baac9c88e5380e1371749509d8fa782
Ellin Tj <tjellin@gmail.com>
2020-02-07 18:13:50 -0800

    figuring out how the ajax works

--- Assets/script.js
+++ Assets/script.js
@@ -2,14 +2,16 @@
 // let inputCity = "";
   
 $(document).ready(function () { 
+
     $(".button").on("click", function() {
+        debugger;
+
         console.log("Button clicked!");
 
-        let inputCity = $(".searchField").value();
+        let inputCity = $(".searchField");
         console.log("inputCity = " + inputCity);
         let url = "https://api.openweathermap.org/data/2.5/weather?q="+inputCity+"&APPID=4c5b7de512dad1fed533c8bdb4858956";
         console.log("url = " + url);
-        debugger;
 
         
         $.ajax({
