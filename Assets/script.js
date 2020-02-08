6b3d120479c3b555b53e9e39787f9cdefea15923
Ellin Tj <tjellin@gmail.com>
2020-02-07 18:31:46 -0800

    adding ajax

--- Assets/script.js
+++ Assets/script.js
@@ -6,18 +6,18 @@ $(document).ready(function () {
 
         let inputCity = $(".searchCity").val();
         console.log("inputCity = " + inputCity);
-        // let url = "https://api.openweathermap.org/data/2.5/weather?q="+inputCity+"&APPID=4c5b7de512dad1fed533c8bdb4858956";
-        // console.log("url = " + url);
+        let url = "https://api.openweathermap.org/data/2.5/weather?q="+inputCity+"&APPID=4c5b7de512dad1fed533c8bdb4858956";
+        console.log("url = " + url);
 
         
-        // $.ajax({
-        //     url,
-        //     method: "GET"
-        // }).then (function(response) {
-        //     console.log("success! Below is the response!!!");
-        //     console.log(response);
+        $.ajax({
+            url,
+            method: "GET"
+        }).then (function(response) {
+            console.log("success! Below is the response!!!");
+            console.log(response);
     
-        // });
+        });
 
         event.preventDefault();
 
