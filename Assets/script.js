80f89ef8c0e29bde51e4ef7b442b90c1153ff51d
Ellin Tj <tjellin@gmail.com>
2020-02-07 18:31:05 -0800

    solve how to read the user input in a text field

--- Assets/script.js
+++ Assets/script.js
@@ -4,20 +4,20 @@ $(document).ready(function () {
     $("button").on("click", function(event) {
         console.log("Button clicked!");
 
-        let inputCity = $(".searchCity");
+        let inputCity = $(".searchCity").val();
         console.log("inputCity = " + inputCity);
-        let url = "https://api.openweathermap.org/data/2.5/weather?q="+inputCity+"&APPID=4c5b7de512dad1fed533c8bdb4858956";
-        console.log("url = " + url);
+        // let url = "https://api.openweathermap.org/data/2.5/weather?q="+inputCity+"&APPID=4c5b7de512dad1fed533c8bdb4858956";
+        // console.log("url = " + url);
 
         
-        $.ajax({
-            url,
-            method: "GET"
-        }).then (function(response) {
-            console.log("success! Below is the response!!!");
-            console.log(response);
+        // $.ajax({
+        //     url,
+        //     method: "GET"
+        // }).then (function(response) {
+        //     console.log("success! Below is the response!!!");
+        //     console.log(response);
     
-        });
+        // });
 
         event.preventDefault();
 
