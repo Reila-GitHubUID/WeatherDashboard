d11e566ab363d72b4a8dff6afc993ba2ef1a2579
Ellin Tj <tjellin@gmail.com>
2020-02-12 13:42:56 -0800

    understanding how weather forecast's time is doing, and how I can integrate it with momentsjs

--- Assets/script.js
+++ Assets/script.js
@@ -8,11 +8,12 @@ $(document).ready(function () {
         inputCity = $(".searchCity").val();
         selectCity = inputCity;
 
+        // put and display previous searches
+        //localStorage.setItem(inputCity, url);   
+        // addToList(inputCity);
+
         let url = "https://api.openweathermap.org/data/2.5/weather?q="+inputCity+"&units=imperial&APPID=4c5b7de512dad1fed533c8bdb4858956";
         displayWeather(url);
-
-        // put and display previous searches
-        localStorage.setItem(inputCity, url);        
 
         event.preventDefault();
     }); // ---- end of button click event listener
@@ -44,19 +45,25 @@ $(document).ready(function () {
                 method: "GET"
             }).then (function (r) {
                 console.log(r);
-                let ISODate = r.date_iso;
-                console.log("ISODate===" + ISODate);
-                let localDate = moment(ISODate).tz("America/Los_Angeles");
-                console.log("localDate= "+ localDate);
+                // let ISODate = r.date_iso;
+                // console.log("ISODate===" + ISODate);
+                // let localDate = moment(ISODate).tz("America/Los_Angeles");
+                // console.log("localDate= "+ localDate);
 
-                let justDate = r.date;
-                console.log("justDate===" + justDate);
-                let justLocalDate = moment(justDate).tz("America/Los_Angeles");
-                console.log("justLocalDate= "+ justLocalDate);
+                // let justDate = r.date;
+                // console.log("justDate===" + justDate);
+                // let justLocalDate = moment(justDate).tz("America/Los_Angeles");
+                // console.log("justLocalDate= "+ justLocalDate);
 
-
-                let date = localDate.format("YYYY-MM-DD HH:mm:ss");
+                console.log("response.dt="+ response.dt);
+                let m = moment(response.dt, 's');
+                console.log("m="+ m);
+                let date = m.format("YYYY-MM-DD HH:mm:ss");
                 console.log("date=" + date);
+
+
+
+
 
                 let uv = r.value;
 
@@ -67,8 +74,7 @@ $(document).ready(function () {
                 $(".uv").text(uv);
     
 
-                // the 5 days forecast
-                
+                // display the 5 days forecast                
                 let urlForecast = "https://api.openweathermap.org/data/2.5/forecast?q="+inputCity+"&units=imperial&APPID=4c5b7de512dad1fed533c8bdb4858956";
                 $.ajax({
                     url: urlForecast,
@@ -77,14 +83,23 @@ $(document).ready(function () {
                     console.log("+++++++++++++++++++++++++++++++");
                     console.log(r);
 
-
+                    let fTime = r.list[9].dt_txt;
+                    console.log("r.list[9].dt_txt==" + fTime);
                 });
             });
 
 
         });
-    } // end of displayWeather function
+    } // ---- end of displayWeather function
 
+
+    function addToList(cityName) {
+        console.log("I'm in the addToList function");
+        console.log("cityName========"+cityName);
+        let newDiv = $("<div>").addClass("row").text(cityName);
+        // newDiv.text(cityName);
+        $(".searchHistory").append(newDiv);
+    } // ---- end of addToList function
 
 
 });
